c873d53c84c2b5cd46db2f50686d16f57c639362
hassankhodabakhshi <hasankhodabakhshi@gmail.com>
2024-10-28 13:03:53 +0330

    توضیح کوتاه درباره تغییرات

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,1 @@
-// Copyright hassankhodabakhshi  2021,2022. All Rights Reserved.
-// Project: folio "https://www.linkedin.com/in/hassan-khodabakhshi-495743a0/",
-// Author contact: 
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+postcs.js
